56a975bc42fe6664a1280a698e57146f8fcb7a05
viktor sapa <sapa.viktor.nik@gmail.com>
2026-09-26 17:57:14 +0700

    update settings.py and urls.py

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email
